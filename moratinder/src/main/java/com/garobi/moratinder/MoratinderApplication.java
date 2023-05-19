0c963c3cf6927b299c734773af7026a2ac4c0f29
garobi <gabriel.rbittencourt15@gmail.com>
2023-05-19 15:24:17 -0300

    Commit Inicial

---
+++ moratinder/src/main/java/com/garobi/moratinder/MoratinderApplication.java
@@ -0,0 +1,13 @@
+package com.garobi.moratinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoratinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoratinderApplication.class, args);
+	}
+
+}
